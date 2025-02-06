19ac71cfda705994dab5af9e076dcdf6d305b65f
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-06 12:30:46 +0530

    useEffect added

--- Digital Clock/src/DigitalClock.jsx
+++ Digital Clock/src/DigitalClock.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react"
+
 function DigitalClock() {
+  cont[(time, setTime)] = useState(new Date())
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    //cleaner function
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
+
+  function formatTime() {}
   return (
     <div className="clock-container">
       <div className="clock">
